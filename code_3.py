1c7fafa77b40d0f11ce394b0cae766c304f68729
Fidel <146652562+fidel222@users.noreply.github.com>
2023-12-17 12:29:06 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,45 +1,17 @@
-        console.log("Random Number: " + item);
-}
-    const data = generateRandomData();
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-    for (let i = 0; i < 10; i++) {
-    });
-    const data = [];
-function main() {
-main();
-    data.forEach(item => {
 
 function generateRandomData() {
-    }
+function main() {
+    });
+        console.log("Random Number: " + item);
+}
+    const data = [];
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+}
 
+
+main();
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
     return data;
-}        }
-    }
-        }
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {
-}
-        int[] data = new int[10];
-
-public class RandomDataGenerator {
-
-        for (int i = 0; i < 10; i++) {    return data;
-function main() {
-}
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-main();
-    }
-    for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
-
-
-    const data = generateRandomData();
-function generateRandomData() {
-    const data = [];
-    });
-
-        console.log("Random Number: " + item);
